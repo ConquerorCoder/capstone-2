7849492c1ffce749bdf902cfd4be47950de1aaf1
ConquerorCoder <Officialchristiandaniel3@gmail.com>
2025-11-12 00:35:14 -0600

    added getPrice() for GarlicKnots

--- src/main/java/com/pluralsight/GarlicKnots.java
+++ src/main/java/com/pluralsight/GarlicKnots.java
@@ -1,4 +1,24 @@
 package com.pluralsight;
+public class GarlicKnots implements OrderItem {
+    private int quantity;
 
-public class GarlicKnots {
+    public GarlicKnots(int quantity) {
+        this.quantity = quantity;
+    }
+
+    public double getPrice() {
+        double price = 4.99;
+        return price * quantity;
+    }
+
+    @Override
+    public String toString() {
+        return "GarlicKnots: " + quantity;
+    }
+
+    public String getDescription() {
+        return quantity + " Garlic Knots";
+
+
+    }
 }
--- src/main/java/com/pluralsight/Main.java
+++ src/main/java/com/pluralsight/Main.java
@@ -4,9 +4,8 @@ package com.pluralsight;
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        Pizza pizza = new Pizza("small","stuffed crust");
-        pizza.setStuffedCrust(true);
-        System.out.println(pizza);
-        System.out.println(pizza.getPrice());
+        GarlicKnots garlicKnots =  new GarlicKnots(3);
+        System.out.println(garlicKnots);
+        System.out.println(garlicKnots.getPrice());
     }
 }
